5f359e9863c6e8db5f3abf8350ae9411edcb6520
X-jelly18 <elitegamer128k@gmail.com>
2026-03-15 20:17:19 +0200

    Update proxy.ts

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "https://pluto.ayanakojixxx.shop" +
+    "https://pluto.ayanakojivps.shop" +
     url.pathname +
     url.search;
 
