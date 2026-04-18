199ab7ce7de87035d9fcce75160016e4a2d8af41
X-jelly18 <elitegamer128k@gmail.com>
2026-04-18 12:31:15 +0200

    Fix backend URL concatenation in proxy handler

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "https://rich.ayanakojivps.shop" +
+    "https://south.ayanakojivps.shop" +
     url.pathname +
     url.search;
 
